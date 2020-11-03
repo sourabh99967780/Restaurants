e86a52018f3ed2967166aa9adf89e9a71ef2df4f
Sourabh Kumar <sourabh99967780@gmail.com>
2020-11-03 21:23:26 +0530

    initial commit

---
+++ components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    <p>{{ info }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    info: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/restaurants.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Restaurants</h2>
+    <AppRestaurantInfo />
+  </div>
+</template>
+
+<script>
+import AppRestaurantInfo from '@/components/AppRestaurantInfo'
+export default {
+  AppRestaurantInfo,
+}
+</script>
+
+<style lang="scss" scoped></style>
